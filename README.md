4b4c24ae65157d371f37f5d06868da6ecd70487a
Nohim Jayasinghe <nohimj@gmail.com>
2026-02-04 20:32:34 -0800

    Initial project skeleton: folders, headers, Orderbook.cpp, and build setup ordertypes and others will be separated later

---
+++ README.md
@@ -0,0 +1,4 @@
+# Low-Latency C++ Trading Engine
+
+Event-driven trading engine with a custom limit order book
+and deterministic replay.
---
+++ src/orderbook.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <deque>
+
+//class for labeling specific order types
+enum class OrderType     
+{
+    GoodTillCancel,
+    FillAndKill
+};
+
+enum class Side
+{
+    Buy,
+    Sell
+
+};
+
+
+
+int main()
+{
+
+    return 0;
+}
