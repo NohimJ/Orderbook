e5d5d976d95b1c4408fcb8be55fa294afb7ad841
Nohim Jayasinghe <nohimj@gmail.com>
2026-05-18 13:46:55 -0400

    Added order modification, from tutorial

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -4,6 +4,10 @@
 #include <unordered_map>
 #include <deque>
 #include <list>
+#include <map>
+#include <memory>
+#include <algorithm>
+#include <stdexcept>
 
 //class for labeling specific order types
 enum class OrderType     
@@ -53,6 +57,7 @@ public:
 
 class Order
 {
+public:
     Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
         : orderType_{ orderType }
         , orderId_{ orderId }
@@ -62,12 +67,14 @@ class Order
         , remainingQuantity_{ quantity }
     { }
 
+    OrderType GetOrderType() const {return orderType_;}
     OrderId GetOrderId() const {return orderId_;}
     Side GetSide() const {return side_; }
     Price GetPrice() const { return price_; }
     Quantity GetInitialQuantity() const { return initialQuantity_; }
     Quantity GetRemainingQuantity() const { return remainingQuantity_; }
     Quantity GetFilledQuantity() const { return GetInitialQuantity() - GetRemainingQuantity(); }
+    bool isFilled() const { return GetRemainingQuantity() == 0; }
     void Fill(Quantity quantity)
     {
         if(quantity > GetRemainingQuantity())
@@ -88,6 +95,188 @@ private:
 using OrderPointer = std::shared_ptr<Order>;  //order can be both stored in a dict (price, ticker) or bid ask based dict
 using OrderPointers = std::list<OrderPointer>;
 
+class ModifyOrder
+{
+public:
+    ModifyOrder(OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderId_{ orderId }
+        , price_{ price }
+        , side_{ side }
+        , quantity_{ quantity }
+    { }
+
+    OrderId GetOrderId() const { return orderId_; }
+    Price GetPrice() const  { return price_; }      //APIS
+    Side GetSide() const { return side_; }
+    Quantity GetQuantity() const { return quantity_; }
+
+    OrderPointer ToOrderPointer(OrderType type) const //currently only works with GoodTillCancel, but want to futureproof code if we add more order types in the future
+    {
+        return std::make_shared<Order>(type, GetOrderId(), GetSide(), GetPrice(), GetQuantity());
+    }
+
+private:
+    OrderId orderId_;
+    Price price_;
+    Side side_;
+    Quantity quantity_;
+};
+
+struct TradeInfo
+{
+    OrderId orderId_;
+    Price price_;
+    Quantity quantity_;
+};
+
+class Trade
+{
+public:
+    Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+        : bidTrade_ { bidTrade }
+        , askTrade_ { askTrade }
+    { }
+
+    const TradeInfo& GetBidTrade() const { return bidTrade_; }
+    const TradeInfo& GetAskTrade() const { return askTrade_; }
+    
+private:
+    TradeInfo bidTrade_;
+    TradeInfo askTrade_;
+};
+
+using Trades = std::vector<Trade>; //return a vector of trades, (can be different order types in the future)
+
+class Orderbook
+{
+private:
+
+    struct OrderEntry
+    {
+        OrderPointer order_{ nullptr };
+        OrderPointers::iterator location_;
+
+    };
+
+    std::map<Price, OrderPointers, std::greater<Price>> bids_;
+    std::map<Price, OrderPointers, std::less<Price>> asks_;
+    std::unordered_map<OrderId, OrderEntry> orders_;
+
+    bool CanMatch(Side side, Price price) const
+    {
+        if (side == Side::Buy)
+        {
+            if(asks_.empty())   //if no asks, can't match, so return false
+                return false;
+            
+            const auto& [bestAsk, _] = *asks_.begin();
+            return price >= bestAsk; 
+        }
+        else
+        {
+            if(bids_.empty())
+                return false;
+            
+            const auto& [bestBid, _] = *bids_.begin();
+            return price <= bestBid;
+        }
+    }
+
+    Trades MatchOrders()
+    {
+        Trades trades;
+        trades.reserve(orders_.size());
+        
+        while (true)
+        {
+            if (bids_.empty() || asks_.empty())
+                break;
+
+            auto& [bidPrice, bids] = *bids_.begin();
+            auto& [askPrice, asks] = *asks_.begin();
+
+            if (bidPrice < askPrice)
+                break;
+                
+            while (bids.size() && asks.size())
+            {
+                auto& bid = bids.front();
+                auto& ask = asks.front();
+
+                Quantity quantity = std::min(bid->GetRemainingQuantity(), ask->GetRemainingQuantity());
+
+                bid->Fill(quantity);
+                ask->Fill(quantity);
+
+                if (bid->IsFilled())
+                {
+                    bids.pop_front();
+                    orders_.erase(bid->GetOrderId());   //if order filled, not order anymore, so erase OrderId of that specific order
+                }
+
+                if (ask->IsFilled())
+                {
+                    asks.pop_front();
+                    orders_.erase(ask->GetOrderId());
+                }
+
+                if (bids.empty())
+                    bids_.erase(bidPrice);
+
+                if (asks.empty())
+                    asks_.erase(askPrice);
+
+                trades.push_back(Trade{ 
+                    TradeInfo{ bid->GetOrderId(), bid->GetPrice(), quantity },
+                    TradeInfo{ ask->GetOrderId(), ask->GetPrice(), quantity}
+                    });
+
+            }
+                
+        }
+
+        if (!bids_.empty())
+        {
+            auto& [_, bids] = *bids_.begin();
+            auto& order = bids.front();
+            if (order->GetOrderType() == OrderType::FillAndKill)
+                CancelOrder(order->GetOrderId());
+        }
+
+        if (!asks_.empty())
+        {
+            auto& [_, asks] = *asks_.begin();
+            auto& order = asks.front();
+            if (order->GetOrderType() == OrderType::FillAndKill)
+                CancelOrder(order->GetOrderId());
+        }
+
+        return trades;
+    }
+
+public:
+
+    Trades AddOrder(OrderPointer order)
+    {
+        if (orders_.contains(order->GetOrderId()))
+            return { };
+        
+        if (order->GetOrderType() == OrderType::FillAndKill && !CanMatch(order->GetSide(), order->GetPrice()))
+            return { };
+
+        OrderPointers::iterator iterator;
+
+        if (order->GetSide() == Side::Buy)
+        {
+            auto& orders = bids_[order->GetPrice()];
+            orders.push_back(order);
+            iterator = std::next(orders.begin(), orders.size()-1);
+        }
+    }
+
+
+
+};
 
 
 int main()
